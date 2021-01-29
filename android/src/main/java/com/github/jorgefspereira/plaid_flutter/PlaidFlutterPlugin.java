22754b232f76b14b538fb1283c5ec80f70984c4a
Jorge Pereira <jorgefspereira@icloud.com>
2021-01-29 16:56:02 +0000

    Upgraded Android SDK to 3.2.2

--- android/src/main/java/com/github/jorgefspereira/plaid_flutter/PlaidFlutterPlugin.java
+++ android/src/main/java/com/github/jorgefspereira/plaid_flutter/PlaidFlutterPlugin.java
@@ -17,25 +17,19 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.BinaryMessenger;
 
 import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
 
 import com.plaid.link.Plaid;
-import com.plaid.link.configuration.LinkConfiguration;
-import com.plaid.link.configuration.LinkLogLevel;
+import com.plaid.link.configuration.LinkPublicKeyConfiguration;
 import com.plaid.link.configuration.LinkTokenConfiguration;
 import com.plaid.link.configuration.PlaidEnvironment;
 import com.plaid.link.configuration.PlaidProduct;
-import com.plaid.link.event.LinkEvent;
-import com.plaid.link.event.LinkEventMetadata;
 import com.plaid.link.result.LinkAccount;
+import com.plaid.link.result.LinkAccountSubtype;
 import com.plaid.link.result.LinkError;
-import com.plaid.link.result.LinkExit;
 import com.plaid.link.result.LinkExitMetadata;
-import com.plaid.link.result.LinkSuccess;
-import com.plaid.link.result.LinkSuccess.LinkSuccessMetadata;
-import com.plaid.link.result.PlaidLinkResultHandler;
-
-import org.json.JSONObject;
+import com.plaid.link.event.LinkEventMetadata;
+import com.plaid.link.result.LinkSuccessMetadata;
+import com.plaid.link.result.LinkResultHandler;
 
 /** PlaidFlutterPlugin */
 public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
@@ -43,62 +37,58 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
   private static final String CHANNEL_NAME = "plugins.flutter.io/plaid_flutter";
 
   /// LinkConfiguration
+  private static final String PUBLIC_KEY = "publicKey";
+  private static final String TOKEN = "token";
   private static final String CLIENT_NAME = "clientName";
   private static final String PRODUCTS = "products";
-  private static final String ENV = "env";
+  private static final String ENVIRONMENT = "environment";
   private static final String ACCOUNT_SUBTYPES = "accountSubtypes";
-  private static final String USER_EMAIL_ADDRESS = "userEmailAddress";
-  private static final String USER_LEGAL_NAME = "userLegalName";
-  private static final String USER_PHONE_NUMBER = "userPhoneNumber";
   private static final String WEBHOOK = "webhook";
   private static final String LINK_CUSTOMIZATION_NAME = "linkCustomizationName";
   private static final String LANGUAGE = "language";
   private static final String COUNTRY_CODES = "countryCodes";
-  private static final String LINK_TOKEN = "linkToken";
-  private static final String PUBLIC_KEY = "publicKey";
-  private static final String PAYMENT_TOKEN = "paymentToken";
+  private static final String USER_EMAIL_ADDRESS = "userEmailAddress";
+  private static final String USER_LEGAL_NAME = "userLegalName";
+  private static final String USER_PHONE_NUMBER = "userPhoneNumber";
+  private static final String TYPE = "type";
+  private static final String SUBTYPE = "subtype";
 
-  /// PlaidLinkResultHandler
-  private static final String ON_SUCCESS_METHOD = "onSuccess";
-  private static final String ON_EXIT_METHOD = "onExit";
-  private static final String ON_EVENT_METHOD = "onEvent";
-  private static final String ERROR = "error";
-  private static final String METADATA = "metadata";
-  private static final String PUBLIC_TOKEN = "publicToken";
-  private static final String EVENT = "event";
+  private static final String LINK_TOKEN_PREFIX = "link-";
+
+  /// LinkResultHandler
+  private static final String METHOD_ON_SUCCESS = "onSuccess";
+  private static final String METHOD_ON_EXIT = "onExit";
+  private static final String METHOD_ON_EVENT = "onEvent";
+  private static final String KEY_ERROR = "error";
+  private static final String KEY_METADATA = "metadata";
+  private static final String KEY_PUBLIC_TOKEN = "publicToken";
+  private static final String KEY_EVENT = "event";
 
   private Activity activity;
   private MethodChannel channel;
 
-  private PlaidLinkResultHandler plaidLinkResultHandler = new PlaidLinkResultHandler(
-      new Function1<LinkSuccess, Unit>() {
-        @Override
-        public Unit invoke(LinkSuccess e) {
-          Map<String, Object> data = new HashMap<>();
+  private LinkResultHandler resultHandler = new LinkResultHandler(
+      linkSuccess -> {
+        Map<String, Object> data = new HashMap<>();
 
-          data.put(PUBLIC_TOKEN, e.getPublicToken());
-          data.put(METADATA, createMapFromConnectionMetadata(e.getMetadata()));
+        data.put(KEY_PUBLIC_TOKEN, linkSuccess.getPublicToken());
+        data.put(KEY_METADATA, mapFromSuccessMetadata(linkSuccess.getMetadata()));
 
-          channel.invokeMethod(ON_SUCCESS_METHOD, data);
-          return Unit.INSTANCE;
-        }
+        channel.invokeMethod(METHOD_ON_SUCCESS, data);
+        return Unit.INSTANCE;
       },
-      new Function1<LinkExit, Unit>() {
-        @Override
-        public Unit invoke(LinkExit e) {
+      linkExit -> {
+        Map<String, Object> data = new HashMap<>();
+        data.put(KEY_METADATA, mapFromExitMetadata(linkExit.getMetadata()));
 
-          Map<String, Object> data = new HashMap<>();
-          data.put(METADATA, createMapFromExitMetadata(e.getMetadata()));
+        LinkError error = linkExit.getError();
 
-          LinkError error = e.getError();
-
-          if(error != null) {
-            data.put(ERROR, error.getErrorMessage());
-          }
-
-          channel.invokeMethod(ON_EXIT_METHOD, data);
-          return Unit.INSTANCE;
+        if(error != null) {
+          data.put(KEY_ERROR, mapFromError(error));
         }
+
+        channel.invokeMethod(METHOD_ON_EXIT, data);
+        return Unit.INSTANCE;
       }
   );
 
@@ -113,36 +103,23 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
   }
 
   private void initializePlugin(Activity activity, BinaryMessenger messenger) {
+    if (channel != null) {
+      channel.setMethodCallHandler(null);
+      channel = null;
+    }
+
     this.activity = activity;
-
-    this.channel = new MethodChannel(messenger, CHANNEL_NAME);
+    channel = new MethodChannel(messenger, CHANNEL_NAME);
     channel.setMethodCallHandler(this);
-
-    Plaid.initialize(activity.getApplication());
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if(call.method.equals("open")) {
-      Plaid.setLinkEventListener(new Function1<LinkEvent, Unit>() {
-        @Override
-        public Unit invoke(LinkEvent e) {
-          Map<String, Object> data = new HashMap<>();
-          data.put(EVENT, e.getEventName().toString());
-          data.put(METADATA, createMapFromEventMetadata(e.getMetadata()));
-
-          channel.invokeMethod(ON_EVENT_METHOD, data);
-          return Unit.INSTANCE;
-        }
-      });
-
-      Map<String, Object> arguments = call.arguments();
-      String linkToken = (String) arguments.get(LINK_TOKEN);
-      LinkConfiguration configuration = linkToken != null && linkToken.startsWith("link-") ? getNewLinkConfiguration(arguments) : getLegacyLinkConfiguration(arguments);
-      Plaid.openLink(activity, configuration);
+      this.open(call.arguments());
     }
     else if(call.method.equals("close")) {
-      
+      this.close();
     }
     else {
       result.notImplemented();
@@ -151,63 +128,128 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
 
   @Override
   public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
-    return plaidLinkResultHandler.onActivityResult(requestCode, resultCode, intent);
+    return resultHandler.onActivityResult(requestCode, resultCode, intent);
   }
 
-  private LinkConfiguration getNewLinkConfiguration(Map<String, Object> arguments) {
-    String linkToken = (String) arguments.get(LINK_TOKEN);
+  private void open(Map<String, Object> arguments) {
+    Plaid.setLinkEventListener( linkEvent -> {
+      Map<String, Object> data = new HashMap<>();
+      data.put(KEY_EVENT, linkEvent.getEventName().toString());
+      data.put(KEY_METADATA, mapFromEventMetadata(linkEvent.getMetadata()));
 
-    LinkTokenConfiguration.Builder configuration = new LinkTokenConfiguration.Builder()
-            .token(linkToken)
-            .logLevel(BuildConfig.DEBUG ? LinkLogLevel.DEBUG : LinkLogLevel.ASSERT);
+      channel.invokeMethod(METHOD_ON_EVENT, data);
+      return Unit.INSTANCE;
+    });
 
-    return configuration.build().toLinkConfiguration();
+    String publicKey = (String) arguments.get(PUBLIC_KEY);
+    String token = (String) arguments.get(TOKEN);
+
+    if (publicKey == null && token == null) {
+      Log.w("PlaidFlutterPlugin", "Token must be part of configuration.");
+      throw new IllegalArgumentException("Token must be part of configuration.");
+    }
+
+    if(publicKey != null) {
+      try {
+        Plaid.create(
+                activity.getApplication(),
+                getLegacyLinkConfiguration(arguments)
+        ).open(activity);
+
+        return;
+      } catch (Exception e) {
+        Log.w("PlaidFlutterPlugin", e.getMessage());
+        throw e;
+      }
+    }
+
+    if(token != null) {
+      Plaid.create(
+              activity.getApplication(),
+              getLinkTokenConfiguration(arguments)
+      ).open(activity);
+      return;
+    }
   }
 
-  private LinkConfiguration getLegacyLinkConfiguration(Map<String, Object> arguments) {
+  private void close() {
 
+  }
+
+  private LinkTokenConfiguration getLinkTokenConfiguration(Map<String, Object> arguments) {
+    String token = (String) arguments.get(TOKEN);
+
+    if (token == null) {
+      return null;
+    }
+
+    if (!token.startsWith(LINK_TOKEN_PREFIX)) {
+      return null;
+    }
+
+    return new LinkTokenConfiguration.Builder()
+            .token(token)
+            .build();
+  }
+
+  private LinkPublicKeyConfiguration getLegacyLinkConfiguration(Map<String, Object> arguments) {
+    // required arguments for configuration
     String clientName = (String) arguments.get(CLIENT_NAME);
     String publicKey = (String) arguments.get(PUBLIC_KEY);
-    String envString = (String) arguments.get(ENV);
+
+    LinkPublicKeyConfiguration.Builder configuration = new LinkPublicKeyConfiguration.Builder()
+            .publicKey(publicKey)
+            .clientName(clientName);
+
+    // optional arguments for configuration
     String language = (String) arguments.get(LANGUAGE);
     String linkCustomizationName = (String) arguments.get(LINK_CUSTOMIZATION_NAME);
     String webhook = (String) arguments.get(WEBHOOK);
     String userLegalName = (String) arguments.get(USER_LEGAL_NAME);
     String userEmailAddress = (String) arguments.get(USER_EMAIL_ADDRESS);
     String userPhoneNumber = (String) arguments.get(USER_PHONE_NUMBER);
-    String paymentToken = (String) arguments.get(PAYMENT_TOKEN);
-    String token = (String) arguments.get(LINK_TOKEN);
+    String token = (String) arguments.get(TOKEN);
+    String environment = (String) arguments.get(ENVIRONMENT);
     ArrayList<String> countryCodes = (ArrayList<String>) arguments.get(COUNTRY_CODES);
-    ArrayList<?> productsObjects = (ArrayList<?>)arguments.get(PRODUCTS);
-    Map<String, ArrayList<String>> accountSubtypes = (Map<String, ArrayList<String>>) arguments.get(ACCOUNT_SUBTYPES);
+    ArrayList<Map<String, String>> accountSubtypes = (ArrayList<Map<String, String>>) arguments.get(ACCOUNT_SUBTYPES);
+    ArrayList<String> products = (ArrayList<String>)arguments.get(PRODUCTS);
 
-    PlaidEnvironment env = PlaidEnvironment.valueOf(envString.toUpperCase());
+    if (products != null) {
+      ArrayList<PlaidProduct> listProducts = new ArrayList<>();
 
-    LinkConfiguration.Builder configuration = new LinkConfiguration.Builder()
-            .publicKey(publicKey)
-            .clientName(clientName)
-            .logLevel(BuildConfig.DEBUG ? LinkLogLevel.DEBUG : LinkLogLevel.ASSERT);
+      for (String item : products) {
+        PlaidProduct p = PlaidProduct.valueOf(item.toUpperCase());
+        listProducts.add(p);
+      }
 
-    ArrayList<PlaidProduct> products = new ArrayList<>();
-
-
-    for (Object po : productsObjects) {
-      String ps = (String)po;
-      PlaidProduct p = PlaidProduct.valueOf(ps.toUpperCase());
-      products.add(p);
+      configuration.products(listProducts);
     }
-
-    configuration.products(products);
 
     if(accountSubtypes != null) {
-      Map<String, String> extraParams = new HashMap<>();
-      JSONObject json = new JSONObject(accountSubtypes);
-      extraParams.put(ACCOUNT_SUBTYPES, json.toString());
-      configuration.extraParams(extraParams);
+      ArrayList<LinkAccountSubtype> listSubtypes = new ArrayList<>();
+
+      for (Map<String, String> item: accountSubtypes) {
+        String type = item.get(TYPE);
+        String subtype = item.get(SUBTYPE);
+        listSubtypes.add(LinkAccountSubtype.Companion.convert(type, subtype));
+      }
+
+      if(!listSubtypes.isEmpty()) {
+        configuration.accountSubtypes(listSubtypes);
+      }
     }
 
-    if(env != null) {
+    if(environment != null) {
+      PlaidEnvironment env = PlaidEnvironment.valueOf(environment.toUpperCase());
       configuration.environment(env);
+    }
+
+    if(countryCodes != null) {
+      configuration.countryCodes(countryCodes);
+    }
+
+    if(token != null) {
+      configuration.token(token);
     }
 
     if(linkCustomizationName != null) {
@@ -218,16 +260,8 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
       configuration.language(language);
     }
 
-    if(countryCodes != null) {
-      configuration.countryCodes(countryCodes);
-    }
-
     if(webhook != null) {
       configuration.webhook(webhook);
-    }
-
-    if(paymentToken != null) {
-      configuration.paymentToken(paymentToken);
     }
 
     if(userLegalName != null) {
@@ -241,54 +275,62 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
     if(userPhoneNumber != null) {
       configuration.userPhoneNumber(userPhoneNumber);
     }
-    
-    if(token != null) {
-      configuration.token(token);
-    }
 
     return configuration.build();
   }
 
-  private Map<String, String> createMapFromEventMetadata(LinkEventMetadata data) {
+  private Map<String, String> mapFromError(LinkError error) {
     Map<String, String> result = new HashMap<>();
 
-    result.put("institution_name", data.getInstitutionName());
-    result.put("mfa_type", data.getMfaType());
-    result.put("request_id", data.getRequestId());
-    result.put("error_message", data.getErrorMessage());
-    result.put("timestamp", data.getTimestamp());
-    result.put("link_session_id", data.getLinkSessionId());
-    result.put("error_code", data.getErrorCode());
-    result.put("exit_status", data.getExitStatus());
-    result.put("institution_id", data.getInstitutionId());
-    result.put("institution_search_query", data.getInstitutionSearchQuery());
-
-    result.put("view_name", data.getViewName().getJsonValue());
-    result.put("error_type", data.getErrorType());
+    result.put("errorType", ""); //TODO:
+    result.put("errorCode", error.getErrorCode() == null ? "" : error.getErrorCode().getJson());
+    result.put("errorMessage", error.getErrorMessage());
+    result.put("errorDisplayMessage", error.getDisplayMessage());
 
     return result;
   }
 
-  private Map<String, Object> createMapFromConnectionMetadata(LinkSuccessMetadata data) {
+  private Map<String, String> mapFromEventMetadata(LinkEventMetadata data) {
+    Map<String, String> result = new HashMap<>();
+
+    result.put("errorType", data.getErrorType());
+    result.put("errorCode", data.getErrorCode());
+    result.put("errorMessage", data.getErrorMessage());
+    result.put("exitStatus", data.getExitStatus());
+    result.put("institutionId", data.getInstitutionId());
+    result.put("institutionName", data.getInstitutionName());
+    result.put("institutionSearchQuery", data.getInstitutionSearchQuery());
+    result.put("linkSessionId", data.getLinkSessionId());
+    result.put("mfaType", data.getMfaType());
+    result.put("requestId", data.getRequestId());
+    result.put("timestamp", data.getTimestamp());
+    result.put("viewName", data.getViewName() == null ? "" : data.getViewName().getJsonValue());
+    result.put("metadataJson", data.getMetadataJson());
+
+    return result;
+  }
+
+  private Map<String, Object> mapFromSuccessMetadata(LinkSuccessMetadata data) {
     Map<String, Object> result = new HashMap<>();
 
     Map<String, String> institution = new HashMap<>();
-    institution.put("name", data.getInstitutionName());
-    institution.put("institution_id", data.getInstitutionId());
+    institution.put("name", data.getInstitution() == null ? "" : data.getInstitution().getName());
+    institution.put("id", data.getInstitution() == null ? "" : data.getInstitution().getId());
 
     result.put("institution", institution);
-    result.put("link_session_id", data.getLinkSessionId());
+    result.put("linkSessionId", data.getLinkSessionId());
+    result.put("metadataJson", data.getMetadataJson());
 
     ArrayList<Object> accounts = new ArrayList<>();
 
     for (LinkAccount a: data.getAccounts()) {
       Map<String, String> aux = new HashMap<>();
-      aux.put("id", a.getAccountId());
-      aux.put("mask", a.getAccountNumber());
-      aux.put("name", a.getAccountName());
-      aux.put("type", a.getAccountType());
-      aux.put("subtype", a.getAccountSubType());
-      aux.put("verification_status", a.getVerificationStatus());
+      aux.put("id", a.getId());
+      aux.put("mask", a.getMask());
+      aux.put("name", a.getName());
+      aux.put("type", a.getSubtype() == null ? "" : a.getSubtype().getAccountType() == null ? "" : a.getSubtype().getAccountType().getJson());
+      aux.put("subtype", a.getSubtype() == null ? "" : a.getSubtype().getJson());
+      aux.put("verificationStatus", a.getVerificationStatus() == null ? "" : a.getVerificationStatus().getJson());
       accounts.add(aux);
     }
 
@@ -297,17 +339,18 @@ public class PlaidFlutterPlugin implements MethodCallHandler, PluginRegistry.Act
     return result;
   }
 
-  private Map<String, Object> createMapFromExitMetadata(LinkExitMetadata data) {
+  private Map<String, Object> mapFromExitMetadata(LinkExitMetadata data) {
     Map<String, Object> result = new HashMap<>();
 
     Map<String, String> institution = new HashMap<>();
-    institution.put("name", data.getInstitutionName());
-    institution.put("institution_id", data.getInstitutionId());
+    institution.put("name", data.getInstitution() == null ? "" : data.getInstitution().getName());
+    institution.put("id", data.getInstitution() == null ? "" : data.getInstitution().getId());
 
     result.put("institution", institution);
-    result.put("request_id", data.getRequestId());
-    result.put("link_session_id", data.getLinkSessionId());
-    result.put("status", data.getExitStatus());
+    result.put("requestId", data.getRequestId());
+    result.put("linkSessionId", data.getLinkSessionId());
+    result.put("status", data.getStatus() == null ? "" : data.getStatus().getJsonValue());
+    result.put("metadataJson", data.getMetadataJson());
 
     return result;
   }
