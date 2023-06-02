fd65690ef5a821a7f529666188468982cc4cd4b7
rs-follow <119872444+rs-follow@users.noreply.github.com>
2023-06-01 22:03:34 -0400

    web jsToMap handles null

--- lib/src/platform/plaid_js_map.dart
+++ lib/src/platform/plaid_js_map.dart
@@ -42,6 +42,10 @@ class WebConfiguration {
 
 /// A workaround to converting an object from JS to a Dart Map.
 Map jsToMap(jsObject) {
+  if (jsObject == null) {
+    return Map(); 
+  }
+  
   return new Map.fromIterable(
     _getKeysOfObject(jsObject),
     value: (key) => getProperty(jsObject, key),
