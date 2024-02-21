fa346783f2c6c793fa8741a8698eea3fa08d3f02
Jorge Pereira <jorgefspereira@icloud.com>
2024-02-21 16:53:16 +0000

    Closes #114

--- lib/plaid_flutter.dart
+++ lib/plaid_flutter.dart
@@ -3,3 +3,4 @@ export 'src/core/link_configuration.dart';
 export 'src/core/metadata.dart';
 export 'src/core/plaid_link.dart';
 export 'src/core/types.dart';
+export 'src/platform/plaid_platform_interface.dart';
